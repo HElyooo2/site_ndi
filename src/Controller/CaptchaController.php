425d6cfc13a2bd1099d368b4b76e4981aab0b304
Helio <helio.gaude02@gmail.com>
2022-12-02 03:50:56 +0100

    Now chow the images generated and you can swap them

--- src/Controller/CaptchaController.php
+++ src/Controller/CaptchaController.php
@@ -11,9 +11,9 @@ use Orhanerday\OpenAi\OpenAi;
 class CaptchaController extends AbstractController
 {
 
-    
 
-    
+
+
 
 
     #[Route('/captcha', name: 'app_captcha')]
@@ -29,14 +29,14 @@ class CaptchaController extends AbstractController
             return substr($string, $ini, $len);
         }
 
-        $open_ai_key = "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";
+        $open_ai_key = "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";
         $open_ai = new OpenAi($open_ai_key);
 
         $complete = $open_ai->complete([
             'engine' => 'text-davinci-003',
-            'prompt' => 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: Write me a stor in 4 steps happening at night.',
+            'prompt' => 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: Write me a simple story in 3 steps.',
             'temperature' => 0.9,
-            'max_tokens' => 1000,
+            'max_tokens' => 3000,
             'frequency_penalty' => 0,
             'presence_penalty' => 0.6,
         ]);
@@ -44,26 +44,29 @@ class CaptchaController extends AbstractController
         $temp = $complete;
 
         $text = get_string_between($complete, '"text"', ',"index"');
-        
-      
+
+
         $stepsString = [
         "step1" => get_string_between($text, 'Step 1: ', 'Step 2:'),
         "step2" => get_string_between($text, 'Step 2: ', 'Step 3:'),
-        "step3" => get_string_between($text, 'Step 3: ', 'Step 4:'),
-        "step4" => get_string_between($text, 'Step 4: ', '"')
+        "step3" => get_string_between($text, 'Step 3: ', '"')
+        // "step4" => get_string_between($text, 'Step 4: ', '"')
         ];
 
-        $stepUrlImages =array();
+        $cpt = 0;
+        $stepUrlImages = array();
         foreach ($stepsString as $cle => $string ){
+            $cpt += 1;
             $completeImage = $open_ai->image([
                 "prompt" => $string,
                 "n" => 1,
                 "size" => "256x256",
                 "response_format" => "url",
              ]);
-             array_push($stepUrlImages, get_string_between($completeImage, '"url": "', '"'));
+             var_dump($completeImage);
+             array_push($stepUrlImages, array(get_string_between($completeImage, '"url": "', '"'), "box".strval($cpt) ));
         }
-        // $temp = $complete;
+        $temp = $stepUrlImages;
 
         //var_dump($stepUrlImages);
         return $this->render('captcha/index.html.twig', [
